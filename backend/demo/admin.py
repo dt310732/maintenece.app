e2a9626d821107bad6c158cc9d5de82e928ad1b3
dt310732 <dt31072@zpsb.pl>
2026-04-27 20:39:12 +0200

    TypeError: type 'ModelAdmin' is not subscriptable

--- backend/demo/admin.py
+++ backend/demo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Message
 
 @admin.register(Message)
-class MessageAdmin(admin.ModelAdmin[Message]):
+class MessageAdmin(admin.ModelAdmin): # type: ignore
     list_display = ("id", "text", "created_at")
     search_fields = ("text",)
-    
+
