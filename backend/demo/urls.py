0c74ed39e2f2cdead0ebb77788f4066f97506478
dt310732 <dt31072@zpsb.pl>
2026-04-28 19:58:22 +0200

    testing

--- backend/demo/urls.py
+++ backend/demo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("ping/", views.ping),
+    path("messages/", views.messages),
 ]
--- backend/demo/views.py
+++ backend/demo/views.py
@@ -1,7 +1,31 @@
+import json
 from django.http import JsonResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+from .models import Message
 
 def ping(request: HttpRequest) -> JsonResponse:
     return JsonResponse({
         'status': 'ok',
         'app': 'demo'
     })
+
+
+@csrf_exempt
+def messages(request: HttpRequest) -> JsonResponse:
+    if request.method == "GET":
+        messages_qs = Message.objects.order_by("-created_at")
+
+        data = [
+            {
+                "id": message.id,
+                "text": message.text,
+                "created_ad": message.created_at.isoformat(),
+            }
+            for message in messages_qs
+        ]
+
+        return JsonResponse(data, safe=False)
+    return JsonResponse(
+        {"error": "Method not allowed"},
+        status=405,
+    )
