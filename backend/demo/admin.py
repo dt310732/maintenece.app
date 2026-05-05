cba86c269da3ee87bed2859602337bf356b77312
dt310732 <dt31072@zpsb.pl>
2026-05-05 19:35:34 +0200

    dev update

--- backend/demo/admin.py
+++ backend/demo/admin.py
@@ -7,7 +7,7 @@ class MessageAdmin(admin.ModelAdmin): # type: ignore
     search_fields = ("text",)
 
 @admin.register(Task)
-class TaskAdmin(Task):
+class TaskAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "is_done", "created_at")
     search_fields = ("title", "description")
     list_filter = ("is_done",)
